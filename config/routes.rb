868d705f33781d15c7c500908d3c4c6b0331d984
yiyukw <yiyu.kwai@kellogg.northwestern.edu>
2022-02-22 21:12:12 +0000

    save\

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-resources :places#index
-resources :posts#index
+resources :places
+resources :posts
   # Defines the root path route ("/")
 
   # root "articles#index"
+  root to: "places#index"
 end
