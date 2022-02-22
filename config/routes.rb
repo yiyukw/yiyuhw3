cbb6d5db033b6e38ed5ad6f41ba25aed885b5fcf
yiyukw <yiyu.kwai@kellogg.northwestern.edu>
2022-02-22 19:22:02 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 resources :articles#index
   # Defines the root path route ("/")
+ # https://3000-yiyukw-yiyuhw3-zz42yyecbq4.ws-us33.gitpod.io/articles
   # root "articles#index"
 end
